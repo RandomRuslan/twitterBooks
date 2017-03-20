8f65a08ae1d6ba77fcd67b1303eb24366a2b48e7
RandomRuslan <random.ruslan@gmail.com>
2017-03-21 00:32:49 +0300

    Second final
    Release split. Testing

--- twitterBooks.py
+++ twitterBooks.py
@@ -2,25 +2,46 @@ import os
 import requests
 import tweepy
 from datetime import datetime, timedelta
+
+def lastSpace(s):
+	for i in range(len(s)):
+		if s[len(s)-i-1] in [" ", "\f", "\n", "\r", "\t", "\v"]:
+			return len(s)-i-1
+	return -1
+
 def updateFile():		#fill cash if it's empty	
 	print("updateFile")
 	fullFile = open("fullText.txt", "r")
-	block = 140*10
-	text = fullFile.read(block)
+	blockSize = 140*10
+	block = ""
+	text = fullFile.read(blockSize+1)
+	
 	if len(text) == 0:
 		fullFile.close()
 		cashFile = open("cashText.txt", "w")
 		cashFile.write(text)
 		cashFile.close()
 		return -1
-
+	
+	for i in range(10):
+		if len(text) < 141:
+			block = block + "[" + str(len(text)) + "]"  + text
+			text = ""
+			break
+		tweet = text[:141]
+		text = tweet[lastSpace(tweet)+1:] + text[141:]
+		tweet = tweet[:lastSpace(tweet)]
+		block = block + "[" + str(len(tweet)) + "]"  + tweet
+	
+	
 	cashFile = open("cashText.txt", "w")
-	cashFile.write(text)
+	cashFile.write(block)
 	cashFile.close()
 	
 	tmpFile = open("tmp.txt", "w")
+	tmpFile.write(text)
 	while True:		
-		text = fullFile.read(block)
+		text = fullFile.read(blockSize)
 		if len(text) == 0:
 			break
 		tmpFile.write(text)
@@ -29,29 +50,32 @@ def updateFile():		#fill cash if it's empty
 	
 	os.remove("fullText.txt")
 	os.rename("tmp.txt", "fullText.txt")	
-	return 0 
+	return 0
 
 def toTweet(api, lastTweet):	#to tweet
 	print("tweet")
 	file = open("cashText.txt", "r")
-	tweet = file.read(140)
+	#tweet = file.read(140)
 	text = file.read()
 	file.close()
 	
-	#check: tweet was here or cashText is empty
-	if(tweet == lastTweet.text or len(tweet) == 0):
-		if len(text) == 0:
-			if updateFile() == -1:
-				file.close()
-				return -1
-			return toTweet(api, lastTweet)
-		elif len(text) < 140:
-			tweet = text
+	if(len(text) == 0): # cashText is empty
+		if updateFile() == -1:
+			return -1
+		return toTweet(api, lastTweet)
+	bracket = text.find("]")
+	lenTweet = int(text[text.find("[")+1:bracket])
+	tweet = text[bracket+1:bracket+1+lenTweet]
+	text = text[bracket+1+lenTweet:]
+	
+	if(tweet == lastTweet.text):	#check: tweet was here
+		if len(text) < 140:
+			tweet = text[bracket+1:]
 			text = ''
 		else:
-			tweet = text[:140]
-			text = text[140:]
-	    
+			lenTweet = int(text[text.find("[")+1:bracket])
+			tweet = text[bracket+1:bracket+1+lenTweet]
+			text = text[bracket+1+lenTweet:]	    
 	api.update_status(tweet) #to tweet
 	
 	if len(text) == 0:
@@ -61,10 +85,9 @@ def toTweet(api, lastTweet):	#to tweet
 		file = open("cashText.txt", "w")
 		file.write(text)
 		file.close()
-	return
+	return 0
 
-if __name__ == "__main__":
-
+def main():
 	#get keys and tokens
 	f = open("const.txt", "r")
 	CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET, TIME_KEY = f.read().split()
@@ -88,9 +111,15 @@ if __name__ == "__main__":
 	}
 	nowTime = requests.get(url, params=params).json()
 	nowTime = datetime.strptime(nowTime["formatted"], "%Y-%m-%d %H:%M:%S")
+
 	if(nowTime - lastTweet.created_at > timedelta(hours=5)):
-		if toTweet(api, lastTweet) == -1:
-			print("The End!")
+		return toTweet(api, lastTweet)
+	
+if __name__ == "__main__":
+	if main() == -1:
+		with open("error.txt", "w") as f:
+			f.write("Error!")
+	
 
 	
 	
