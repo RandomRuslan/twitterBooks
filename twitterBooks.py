55de82138bcd0d219add51e7bda488e44b637e29
RandomRuslan <random.ruslan@gmail.com>
2017-09-21 23:57:42 +0300

    remove check of time

--- twitterBooks.py
+++ twitterBooks.py
@@ -53,11 +53,11 @@ def updateFile():		#fill cash if it's empty
 	return 0
 
 def toTweet(api, lastTweet):	#to tweet
-	print("tweet")
-	file = open("cashText.txt", "r")
-	#tweet = file.read(140)
-	text = file.read()
-	file.close()
+	print("tweet")	
+
+	with open("cashText.txt", "r") as f:
+	    text = f.read()
+	    #tweet = file.read(140)
 	
 	if(len(text) == 0): # cashText is empty
 		if updateFile() == -1:
@@ -89,9 +89,8 @@ def toTweet(api, lastTweet):	#to tweet
 
 def main():
 	#get keys and tokens
-	f = open("const.txt", "r")
-	CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET, TIME_KEY = f.read().split()
-	f.close()
+	with open("const.txt", "r") as f:
+	   CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET = f.read().split()
 	
 	#connect with twitter
 	auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
@@ -101,7 +100,10 @@ def main():
 	tweets = api.user_timeline()
 	lastTweet = tweets[0]
 	
+	return toTweet(api, lastTweet)
+
 	#get real time
+	"""
 	url = "http://api.timezonedb.com/v2/get-time-zone"
 	params = {
 		'key' : TIME_KEY,
@@ -114,6 +116,7 @@ def main():
 
 	if(nowTime - lastTweet.created_at > timedelta(hours=5)):
 		return toTweet(api, lastTweet)
+	"""
 	
 if __name__ == "__main__":
 	if main() == -1:
